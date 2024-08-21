c1b587b14e188a43167481672eada807b1bde394
EldenRickDev <eldenrickdev@gmail.com>
2024-08-21 11:39:41 -0400

    Initial Commit

---
+++ ClickCounter/ClickCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickCounterApp.swift
+//  ClickCounter
+//
+//  Created by Ricardo Andrés Gatica Collarte on 21-08-24.
+//
+
+import SwiftUI
+
+@main
+struct ClickCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
